480b8a5579cfa2347fa6113f2d989a2ee7551078
anmoldtu <34093007+anmoldtu@users.noreply.github.com>
2019-07-13 03:36:57 +0530

    Updates

---
+++ precache-manifest.69d86ead6c8cda77e2ff1934cf7c6d4d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102820658f03b023dc9ef4a0d5a2cd6a",
+    "url": "/rang2/index.html"
+  },
+  {
+    "revision": "5fc7b0f4613275b9c056",
+    "url": "/rang2/static/css/main.3abfa1b3.chunk.css"
+  },
+  {
+    "revision": "2cecaad9fa9b2eb64602",
+    "url": "/rang2/static/js/2.7227ce8d.chunk.js"
+  },
+  {
+    "revision": "5fc7b0f4613275b9c056",
+    "url": "/rang2/static/js/main.910a141b.chunk.js"
+  },
+  {
+    "revision": "79ac403fd94957595a50",
+    "url": "/rang2/static/js/runtime~main.a78fee37.js"
+  },
+  {
+    "revision": "b7b846c5de766aaa7f1b9acc222907d2",
+    "url": "/rang2/static/media/enac.b7b846c5.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rang2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df65bba77e0c590f28872cc64df4ffd4",
+    "url": "/rang2/static/media/rag1.df65bba7.jpg"
+  },
+  {
+    "revision": "91284cc38e6132f4a6350c18755b7cc6",
+    "url": "/rang2/static/media/rag4.91284cc3.jpg"
+  },
+  {
+    "revision": "1b34bb3793a12089719f95e400c86720",
+    "url": "/rang2/static/media/rag5.1b34bb37.jpg"
+  },
+  {
+    "revision": "0eba2008f2d79bb911730f9f87c13f17",
+    "url": "/rang2/static/media/rag6.0eba2008.jpg"
+  },
+  {
+    "revision": "dd94cd1bf191109a318236dcc7d18971",
+    "url": "/rang2/static/media/rang-logo.dd94cd1b.png"
+  }
+]);
